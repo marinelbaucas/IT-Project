6f5a90b7c95548df535fc5cbbf32330f9f96cdcb
marinelbaucas <marinelbaucas@gmail.com>
2017-11-06 15:43:37 +0800

    Add files via upload

---
+++ categoryMain.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+
+<?php
+ 
+
+
+ 
+		
+		echo "
+       
+			<tr>
+				<td>name</td>
+				<td>beginning_inv</td>		
+				<td>stocks</td>	
+				<td>total_quantity</td>
+				<td>request</td>
+				<td>total_stocks</td>
+				<td>price</td>
+				<td>total_cost</td>
+
+			</tr>	
+		";
+ 
+?>
+</body>
+</html>
---
+++ connectDB.php
@@ -0,0 +1,10 @@
+<?php
+
+$con = mysqli_connect("localhost","root","","inventory_bbccc");
+
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+?>
